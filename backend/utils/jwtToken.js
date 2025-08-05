decceb66972dd03b0106b829839acaf56e97dd46
Adarsh Narayan <adarshnarayangumla@gmail.com>
2025-08-06 01:02:24 +0530

    Update CORS configuration to allow localhost for development and adjust sameSite cookie attribute

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -5,7 +5,6 @@ export const sendToken = (user, statusCode, res, message) => {
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true, // Set httpOnly to true
-    sameSite : 'strict',
     sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
     secure: process.env.NODE_ENV === 'production' ? true : false
   };
